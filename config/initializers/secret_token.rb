7a8009e9a85a22c8e226ad4a70bc5193192d1212
Lisa Pscolkoski <lpscol01@gmail.com>
2014-03-11 21:27:20 -0400

    first commit rails new app from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HwApp::Application.config.secret_key_base = '61db8082e699790686212296f2d1ff53d8f79d10076477c272d02e38946281d5d9c4f3661e3e9bb8957a75be7d22a9f8e2f3e9fb8d58269f35c6fab0ed71af3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HwApp::Application.config.session_store :cookie_store, key: '_hw_app_session'
